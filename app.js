40ea40e91518e4e4cba5d91b417ecc1773bacdf0
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2026-03-23 12:25:03 +0200

    feat: add helmet security middleware to protect headers

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app=express();
+app.use(helmet());
+
+app.get('/',(req,res)=>res.send("swiftQueue secure"));
 const PORT=3000;
 
 app.listen(PORT ,()=>{
