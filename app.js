0c93e0ef59b8ead54ebf402d5dbec16f1745f1e5
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2026-03-23 16:37:38 +0200

    fix: remove node_modules from tracking and apply .gitignore

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import helmet from 'helmet';
 
@@ -5,7 +6,7 @@ const app=express();
 app.use(helmet());
 
 app.get('/',(req,res)=>res.send("swiftQueue secure"));
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 
 app.listen(PORT ,()=>{
     console.log(`swift queue server is running on port ${PORT}`);
