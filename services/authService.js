cd49851efb28e8af155502e96a65983fe5caaa64
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2026-04-01 09:51:18 +0200

    authService missing code

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma.js';
+import bcrypt from 'bcryptjs';
 
 export const registerSchema = async (userData) => {
     const hashedPassword = await bcrypt.hash(userData.password, 10);
@@ -54,5 +55,25 @@ export const updateUserProfile=async (userId,updateData)=>{
         }
     });
 };
+//what is the difference between 400 and 401 and 404?
+export const updatePassword=async(userId,currentPassword,newPassword)=>{
+    const user = await prisma.user.findUnique({where:{id:userId}});
+    if(!user) throw new Error("User not found");
+    const isMatch=awiat bcrypt.compare(oldPassword,user.password);
+    if(!isMatch){
+        const error=new Error("Current password is incorrect");
+        error.statusCode=401;
+        throw error;
+    }
+
+    const salt=await bcrypt.genSalt(10);//what is salt and usage 
+    const hashedPassword=await bcrypt.hash(newPassword,salt);
+
+    await prisma.user.update({
+        where:{id:userId},
+        data:{password:hashedPassword}
+    });
+    return true;
+}
 
 // tell me all crud we user after prisma.user.??? update,findUnique...??
