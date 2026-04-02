8a2bd898153f1839de7baf9f80334b2acb3f8b1d
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2026-04-02 13:53:39 +0200

    feat: setup axios instance with JWT interceptors

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,12 +1,17 @@
+import axios from 'axios';
+
 const api = axios.create({
     baseURL:'http://localhost:3000/api',
 });
+ // what do we mean by interceptors ? and when we use them ?
 api.interceptors.request.use((config)=>{
     const token = localStorage.getItem('token');
     if(token){
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
+}, (error)=>{
+    return Promise.reject(error);
 });
 
 export default api;
