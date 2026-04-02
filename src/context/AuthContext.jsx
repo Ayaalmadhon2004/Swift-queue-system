11c2b9d1a6cd834a28c127de237082b84f13ec1c
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2026-04-02 13:59:45 +0200

    feat: implement AuthContext for global user state management

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,26 +1,45 @@
-import { createContext, useState } from "react";
-import api from "../api/axiosConfig";
+import { createContext, useState, useEffect } from 'react';
+import api from '../api/axiosConfig';
 
 export const AuthContext = createContext();
-export const AuthProvider = ({children})=>{
-    const [user,setUser]=useState(null);
-    const [loading,setLoading]=useState(true);
 
-    const login=async(email,password)=>{
-        const {data} = await api.post('/auth/login',{email,password});
-        localStorage.setItem('token',data.token);
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // دالة للتحقق من التوكن عند تشغيل التطبيق لأول مرة
+    useEffect(() => {
+        const checkUser = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                if (token) {
+                    const { data } = await api.get('/auth/me'); // المسار الذي برمجناه في الباك أند
+                    setUser(data.data);
+                }
+            } catch (error) {
+                localStorage.removeItem('token');
+            } finally {
+                setLoading(false);
+            }
+        };
+        checkUser();
+    }, []);
+
+    const login = async (email, password) => {
+        const { data } = await api.post('/auth/login', { email, password });
+        localStorage.setItem('token', data.token);
         setUser(data.user);
     };
 
-    const logout=()=>{
+    const logout = () => {
         // why i am using localStorage here instead of prisma , we use prisma 
         localStorage.removeItem('token');
         setUser(null);
     };
 
-    return(
-        <AuthContext.Provider value={{user,login,logout,loading}}>
+    return (
+        <AuthContext.Provider value={{ user, login, logout, loading }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
